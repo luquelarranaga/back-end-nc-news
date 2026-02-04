a379e712ef856d994518dbe6fcd4c7496ec12968
maria luque larranaga <malula@marias-Air.communityfibre.co.uk>
2026-02-04 17:43:01 +0000

    completed test for articles_id endpoint

--- __tests__/app_tests/app.articles.test.js
+++ __tests__/app_tests/app.articles.test.js
@@ -32,3 +32,33 @@ describe(("/api/articles/"), () => {
         })
     })
 })
+
+describe(("/api/articles/:article_id"), () => {
+    test(("GET 200: responds with the correct article object"), ()=> {
+        return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({body}) => {          
+            const correctShape = body.every((article) => {
+                return typeof article.author === "string" &&
+                    typeof article.title === "string" &&
+                    typeof article.article_id === "number" &&
+                    typeof article.body === "string" &&
+                    typeof article.topic === "string" &&
+                    typeof article.created_at === "string" &&
+                    typeof article.votes === "number" &&
+                    typeof article.article_img_url === "string"
+            }) 
+            expect(correctShape).toBe(true)
+        })
+    })
+    test(("GET 200: article returned corresponds to correct article_id"), ()=> {
+        return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({body}) => {      
+            const isCorrectArticle = body[0].article_id === 1 ? true : false
+            expect(isCorrectArticle).toBe(true)
+        })
+    })
+})
