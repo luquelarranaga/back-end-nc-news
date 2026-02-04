f12cbb10dfafd3d0a2616ca9ae953e209c7badd6
maria luque larranaga <malula@marias-Air.communityfibre.co.uk>
2026-02-04 12:00:21 +0000

    created a routers folder with a topics_router file. Hooked up the topic router within the app file

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.join());
+const topicsRouter = require("./routers/topic_routes")
+
+app.use('/api/topics', topicsRouter);
+
+module.export = app
+
 
 
