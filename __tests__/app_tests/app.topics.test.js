a8ecca5365265c86576f5691db07cd946b0f805f
maria luque larranaga <malula@marias-Air.communityfibre.co.uk>
2026-02-05 14:59:09 +0000

    reformatted topics test by adding extra describe block containing http method and status code

--- __tests__/app_tests/app.topics.test.js
+++ __tests__/app_tests/app.topics.test.js
@@ -1,38 +1,40 @@
 const db = require("../../db/connection");
 const data = require("../../db/data/test-data");
-const seed = require("../../db/seeds/seed")
-const request = require("supertest")
+const seed = require("../../db/seeds/seed");
+const request = require("supertest");
 const app = require("../../app");
 
-beforeEach(()=> {
-    return seed(data)
+beforeEach(() => {
+  return seed(data);
 });
 
 afterAll(() => {
-    return db.end
+  return db.end;
 });
 
-describe(("/api/topics/"), () => {
-    test(("GET 200: responds with an object with a key of topics with a value of an array of objects"), ()=> {
-        return request(app)
+describe("/api/topics/", () => {
+  describe("GET 200", () => {
+    test("responds with an object with a key of topics with a value of an array of objects", () => {
+      return request(app)
         .get("/api/topics/")
         .expect(200)
-        .then(({body}) => {     
-            expect(body).toBeObject()
-            expect(body.topics).toBeArray()
-        })
-    })
-    test(("GET 200: responds with the correct topics object"), ()=> {
-        return request(app)
+        .then(({ body }) => {
+          expect(body).toBeObject();
+          expect(body.topics).toBeArray();
+        });
+    });
+    test("responds with the correct topics object", () => {
+      return request(app)
         .get("/api/topics")
         .expect(200)
-        .then(({body}) => {  
-            const {topics} = body      
-            topics.forEach((topic)=> {
-                expect(typeof topic.slug).toBe("string");
-                expect(typeof topic.description).toBe("string");
-                expect(typeof topic.img_url).toBe("string");
-            })        
-        })
-    })
-})
+        .then(({ body }) => {
+          const { topics } = body;
+          topics.forEach((topic) => {
+            expect(typeof topic.slug).toBe("string");
+            expect(typeof topic.description).toBe("string");
+            expect(typeof topic.img_url).toBe("string");
+          });
+        });
+    });
+  });
+});
