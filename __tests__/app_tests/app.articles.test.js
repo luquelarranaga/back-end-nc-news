d5cad09bdb21da0377daa07a10b15a143b70bd3a
maria luque larranaga <malula@marias-Air.communityfibre.co.uk>
2026-02-05 15:40:44 +0000

    completed invalid path test for articles

--- __tests__/app_tests/app.articles.test.js
+++ __tests__/app_tests/app.articles.test.js
@@ -1,92 +1,103 @@
 const db = require("../../db/connection");
 const data = require("../../db/data/test-data");
-const seed = require("../../db/seeds/seed")
-const request = require("supertest")
+const seed = require("../../db/seeds/seed");
+const request = require("supertest");
 const app = require("../../app");
 
-beforeEach(()=> {
-    return seed(data)
+beforeEach(() => {
+  return seed(data);
 });
 
 afterAll(() => {
-    return db.end
+  return db.end;
 });
 
-describe(("/api/articles/"), () => {
-    test(("GET 200: responds with an object with a key of articles with a value of an array of objects"), ()=> {
-        return request(app)
-        .get("/api/articles/")
-        .expect(200)
-        .then(({body}) => {     
-            expect(body).toBeObject()
-            expect(body.articles).toBeArray()
-        })
-    })
-    test(("GET 200: responds with the correct articles object"), ()=> {
-        return request(app)
-        .get("/api/articles/")
-        .expect(200)
-        .then(({body}) => {   
-            const {articles} = body      
-            articles.forEach((article)=> {
-                expect(typeof article.author).toBe("string");
-                expect(typeof article.title).toBe("string");
-                expect(typeof article.article_id).toBe("number");
-                expect(typeof article.topic).toBe("string");
-                expect(typeof article.created_at).toBe("string");
-                expect(typeof article.votes).toBe("number");
-                expect(typeof article.article_img_url).toBe("string");
-            })         
-        })
-    })
-    test(("GET 200: articles object contains a key of total_comments"), ()=> {
-        return request(app)
-        .get("/api/articles/")
-        .expect(200)
-        .then(({body}) => {   
-            const {articles} = body      
-            articles.forEach((article)=> {
-                expect(typeof article.total_comments).toBe("number");
-            })         
-        })
-    })
-})
+describe("/api/articles/", () => {
+  test("GET 200: responds with an object with a key of articles with a value of an array of objects", () => {
+    return request(app)
+      .get("/api/articles/")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toBeObject();
+        expect(body.articles).toBeArray();
+      });
+  });
+  test("GET 200: responds with the correct articles object", () => {
+    return request(app)
+      .get("/api/articles/")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        articles.forEach((article) => {
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+        });
+      });
+  });
+  test("GET 200: articles object contains a key of total_comments", () => {
+    return request(app)
+      .get("/api/articles/")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        articles.forEach((article) => {
+          expect(typeof article.total_comments).toBe("number");
+        });
+      });
+  });
+});
 
-describe(("/api/articles/:article_id"), () => {
-    test(("GET 200: responds with an object with a key of articles with a value of an object"), ()=> {
-        return request(app)
-        .get("/api/articles/2")
-        .expect(200)
-        .then(({body}) => {     
-            expect(body).toBeObject()
-            expect(body.article).toBeArray()
-        })
-    })
-    test(("GET 200: responds with the correct article object"), ()=> {
-        return request(app)
-        .get("/api/articles/1")
-        .expect(200)
-        .then(({body}) => {  
-            console.log("body of articles/article_id", body)
-            const {article} = body      
-            expect(typeof article[0].author).toBe("string");
-            expect(typeof article[0].title).toBe("string");
-            expect(typeof article[0].article_id).toBe("number");
-            expect(typeof article[0].body).toBe("string");
-            expect(typeof article[0].topic).toBe("string");
-            expect(typeof article[0].created_at).toBe("string");
-            expect(typeof article[0].votes).toBe("number");
-            expect(typeof article[0].article_img_url).toBe("string");       
-        })
-    })
-    test(("GET 200: article returned corresponds to correct article_id"), ()=> {
-        return request(app)
-        .get("/api/articles/1")
-        .expect(200)
-        .then(({body}) => {  
-            const {article} = body    
-            const isCorrectArticle = article[0].article_id === 1 ? true : false
-            expect(isCorrectArticle).toBe(true)
-        })
-    })
-})
+describe("/api/articles/:article_id", () => {
+  test("GET 200: responds with an object with a key of articles with a value of an object", () => {
+    return request(app)
+      .get("/api/articles/2")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toBeObject();
+        expect(body.article).toBeArray();
+      });
+  });
+  test("GET 200: responds with the correct article object", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        console.log("body of articles/article_id", body);
+        const { article } = body;
+        expect(typeof article[0].author).toBe("string");
+        expect(typeof article[0].title).toBe("string");
+        expect(typeof article[0].article_id).toBe("number");
+        expect(typeof article[0].body).toBe("string");
+        expect(typeof article[0].topic).toBe("string");
+        expect(typeof article[0].created_at).toBe("string");
+        expect(typeof article[0].votes).toBe("number");
+        expect(typeof article[0].article_img_url).toBe("string");
+      });
+  });
+  test("GET 200: article returned corresponds to correct article_id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        const isCorrectArticle = article[0].article_id === 1 ? true : false;
+        expect(isCorrectArticle).toBe(true);
+      });
+  });
+});
+
+describe("invalid file path", () => {
+  test("404: invalid file path returns error message", () => {
+    return request(app)
+      .get("/api/invalid-path/")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Path not found!");
+      });
+  });
+});
