d3ef79f2e20353a2a690f94ddaa61dc73b0eff35
maria luque larranaga <malula@marias-MacBook-Air.local>
2026-02-10 16:30:21 +0000

    created necessary files and changes in package.json and connection js file

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,24 @@
-const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-const ENV = process.env.NODE_ENV || 'development'
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
+const config = {};
 
-module.exports = db;
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const { Pool } = require("pg");
+
+module.exports = new Pool(config);
+
+// const db = new Pool();
+
+// module.exports = db;
--- listen.js
+++ listen.js
@@ -1,9 +1,14 @@
-const app = require("./app");
+// const app = require("./app");
 
-app.listen(8000, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Listening on port 8000");
-  }
-});
+// app.listen(8000, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Listening on port 8000");
+//   }
+// });
+
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
