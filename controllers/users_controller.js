cb35406390f9dc59edafed271e98e80f6a43d36e
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-02-20 16:18:06 +0000

    updated path in user controller file to relative path

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,9 @@
-const getAllUsersService = require("/Users/malula/NORTHCODERS/projects/nc-news-BE/services/users_service.js")
+const getAllUsersService = require("../services/users_service.js");
 
 const getAllUsers = async (req, res) => {
-    const users = await getAllUsersService()
+  const users = await getAllUsersService();
 
-    return res.status(200).send({users: users})
-}
+  return res.status(200).send({ users: users });
+};
 
 module.exports = getAllUsers;
